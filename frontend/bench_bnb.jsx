38bbd1b62f295ecc7fda43b451dd94857fdac073
James <james@jameshawkins.com>
2019-02-25 15:38:09 -0800

    Complete Phase 3 of Benchbnb Project

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -5,9 +5,22 @@ import configureStore from './store/store'
 import Root from "./components/root"
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  const store = configureStore();
+  let store;
   // debugger
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+        },
+      session: { 
+        currentUser: window.currentUser.id }
+      };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 
   window.APIUtil = APIUtil;
